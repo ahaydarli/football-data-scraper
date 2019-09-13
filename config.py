92fbba21c192dae58d8b6b1fcae6d7d33119eb2f
Ali Heyderli <ali.heyderli@gmail.com>
2019-09-14 01:27:01 +0400

    change

--- config.py
+++ config.py
@@ -5,6 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key'
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/bet'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://doadmin:z9ydmc15oc1c91b1@db-postgresql-fra1-41641-do-user-6517766-0.db.ondigitalocean.com:25060/defaultdb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-
